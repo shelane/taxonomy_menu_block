737487fe9bd557e4919725ea57a847bf8e3cfdea
Sam Spinoy <sam.spinoy@gmail.com>
2013-03-31 15:13:44 +0200

    Coding standards

--- taxonomy_menu_block.api.php
+++ taxonomy_menu_block.api.php
@@ -7,18 +7,18 @@
 
 /**
  * Alter data of the taxonomy tree.
- * 
+ *
  * Alter data of the taxonomy tree before the tree gets nested and sent to the
  * theme function.
  *
  * @param $tree
- *   A flat array of taxonomy terms, keyed by their tid. It's very important 
- *   the keys of this array never get reset, as they are used for nesting the 
+ *   A flat array of taxonomy terms, keyed by their tid. It's very important
+ *   the keys of this array never get reset, as they are used for nesting the
  *   tree, later in the Taxonomy Menu Block module.
  *   The order of the terms is still the same as when returned by the
  *   taxonomy_get_tree() function.
- *  @param $config
- *    An array containing configuration of current block.
+ * @param $config
+ *   An array containing configuration of current block.
  *
  */
 function hook_taxonomy_tree_alter(&$tree, &$config) {
@@ -40,9 +40,9 @@ function hook_taxonomy_tree_alter(&$tree, &$config) {
  * shown. The active trail is also based on this active tid
  *
  * Currently only taxonomy (taxonomy/term/%) and node (node/%) pages are
- * supported. On taxonomy pages the active tid is always the tid of the currently
- * viewed term, on node pages the active tid is the value of any term reference
- * field (if any) that refers to the vocabulary we need to display.
+ * supported. On taxonomy pages the active tid is always the tid of the
+ * currently viewed term, on node pages the active tid is the value of any term
+ * reference field (if any) that refers to the vocabulary we need to display.
  *
  * @param $tid
  *   The currently active tid.
@@ -50,7 +50,7 @@ function hook_taxonomy_tree_alter(&$tree, &$config) {
  *   An array containing configuration of current block.
  */
 function hook_active_tid_alter(&$tid, &$config) {
-  // Add support for a custom page we have defined in our Drupal setup, for 
+  // Add support for a custom page we have defined in our Drupal setup, for
   // example with Views: www.site.com/custom/page/tid.
   if (arg(0) == 'custom' && arg(1) == 'page') {
     $tid = arg(2);
