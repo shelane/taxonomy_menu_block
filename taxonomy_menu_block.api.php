c6f28cf18918879bcfd09cf1e823a52a03e55057
Sam Spinoy <sam.spinoy@gmail.com>
2013-03-31 15:04:12 +0200

    Coding standards update

--- taxonomy_menu_block.api.php
+++ taxonomy_menu_block.api.php
@@ -6,28 +6,29 @@
  */
 
 /**
+ * Alter data of the taxonomy tree.
+ * 
  * Alter data of the taxonomy tree before the tree gets nested and sent to the
- * theme function
+ * theme function.
  *
- * @param
- *  $tree
- *    A flat array of taxonomy terms, keyed by their tid. It's very important the
- *    keys of this array never get reset, as they are used for nesting the tree,
- *    later in the Taxonomy Menu Block module.
- *    The order of the terms is still the same as when returned by the
- *    taxonomy_get_tree() function.
- *  $config
- *    An array containing configuration of current block
+ * @param $tree
+ *   A flat array of taxonomy terms, keyed by their tid. It's very important 
+ *   the keys of this array never get reset, as they are used for nesting the 
+ *   tree, later in the Taxonomy Menu Block module.
+ *   The order of the terms is still the same as when returned by the
+ *   taxonomy_get_tree() function.
+ *  @param $config
+ *    An array containing configuration of current block.
  *
  */
 function hook_taxonomy_tree_alter(&$tree, &$config) {
-  // Add the number of nodes associated with each term
+  // Add the number of nodes associated with each term.
   foreach ($tree as $tid => $term) {
     $nodes = db_select('taxonomy_index', 'ti')
-            ->condition('tid', (int) $tid)
-            ->countQuery()
-            ->execute()
-            ->fetchField();
+        ->condition('tid', (int) $tid)
+        ->countQuery()
+        ->execute()
+        ->fetchField();
     $tree[$tid]['name'] = $term['name'] . ' (' . $nodes . ')';
   }
 }
@@ -43,15 +44,14 @@ function hook_taxonomy_tree_alter(&$tree, &$config) {
  * viewed term, on node pages the active tid is the value of any term reference
  * field (if any) that refers to the vocabulary we need to display.
  *
- * @param
- *  $tid
- *    The currently active tid
- *  $config
- *    An array containing configuration of current block
+ * @param $tid
+ *   The currently active tid.
+ * @param $config
+ *   An array containing configuration of current block.
  */
 function hook_active_tid_alter(&$tid, &$config) {
-  // Add support for a custom page we have defined in our Drupal setup, for example
-  // with Views: www.site.com/custom/page/tid
+  // Add support for a custom page we have defined in our Drupal setup, for 
+  // example with Views: www.site.com/custom/page/tid.
   if (arg(0) == 'custom' && arg(1) == 'page') {
     $tid = arg(2);
   }
